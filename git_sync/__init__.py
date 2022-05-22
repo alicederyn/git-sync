38e97a516be2cebcbedf4db4215157c5f2be3cd9
Alice Purcell <Alice.Purcell.39@gmail.com>
2022-05-22 15:01:07 +0100

    Port original git-sync script from bash
    The goal is to get the local repository fully in-sync with all remotes, including fast-forwarding the local copy of branches like "main". Additionally, if there are different origin and upstream remotes, keep origin's copy of main tracking upstream as well, as this avoids occasional mistakes.
    
    This implementation assumes the only local branch that needs to track its remote upstream is called "main", and that the remote origin copy of main needs to be kept up-to-date.

--- git_sync/__init__.py
+++ git_sync/__init__.py
@@ -1,2 +1,20 @@
+from subprocess import check_call, check_output
+
+
+def get_current_branch() -> bytes:
+    return check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]).strip()
+
+
+def get_main_upstream() -> bytes:
+    return check_output(["git", "rev-parse", "--abbrev-ref", "main@{u}"]).strip()
+
+
 def main() -> None:
-    print("Hello World!")
+    current_branch = get_current_branch()
+    if current_branch == b"main":
+        check_call(["git", "pull", "--all"])
+    else:
+        check_call(["git", "fetch", "--all"])
+        main_upstream = get_main_upstream()
+        check_call(["git", "fetch", ".", main_upstream + b":main"])
+    check_call(["git", "push", "origin", "main"])
