alicederyn/git-sync

commit f4cf13a8fffb0042c5dbf5884c97de8d3a53e4ba

Merge pull/fetch into fastforward function

git_sync/git.py
@@ -38,7 +38,11 @@ def get_remote_branches(remote: bytes) -> List[bytes]:
     ).splitlines()
 
 
-def fast_forward_to_upstream(branches: Iterable[Branch]) -> None:
+def fetch_and_fast_forward_to_upstream(branches: Iterable[Branch]) -> None:
+    if any(b.is_current for b in branches):
+        check_call(["git", "pull", "--all"])
+    else:
+        check_call(["git", "fetch", "--all"])
     fetch_args = [b.upstream + b":" + b.name for b in branches]
     if fetch_args:
         # Use run rather than check_call as fetch will exit with a non-zero code

git_sync/__init__.py
@@ -1,9 +1,9 @@
 import sys
-from subprocess import CalledProcessError, check_call
+from subprocess import CalledProcessError
 
 from .git import (
     fast_forward_to_downstream,
-    fast_forward_to_upstream,
+    fetch_and_fast_forward_to_upstream,
     get_branches_with_remote_upstreams,
 )
 
@@ -15,9 +15,5 @@ def main() -> None:
         # Probably not in a git directory, or some other misconfiguration.
         # git will already have written the problem to stderr.
         sys.exit(e.returncode)
-    if any(b.is_current for b in branches):
-        check_call(["git", "pull", "--all"])
-    else:
-        check_call(["git", "fetch", "--all"])
-    fast_forward_to_upstream(b for b in branches if not b.is_current)
+    fetch_and_fast_forward_to_upstream(branches)
     fast_forward_to_downstream(branches)

commit 6bd02c11a9e35145191ab9d83956aaeb05396520

Do not fetch current branch
Fix bug introduced by f4cf13a8fffb0042c5dbf5884c97de8d3a53e4ba, where the current branch was no longer being filtered out of the list of branches to update with fetch after doing a git pull.

git_sync/git.py
@@ -123,7 +123,7 @@ async def fetch_and_fast_forward_to_upstream(branches: Iterable[Branch]) -> None
         await git("pull", "--all")
     else:
         await git("fetch", "--all")
-    fetch_args = [b.upstream + b":" + b.name for b in branches]
+    fetch_args = [b.upstream + b":" + b.name for b in branches if not b.is_current]
     if fetch_args:
         # Ignore return code as fetch will exit with a non-zero code if any
         # branch cannot be fast-forwarded, which is not a failing state for

pyproject.toml
@@ -1,6 +1,6 @@
 [tool.poetry]
 name = "git-sync"
-version = "0.2.0"
+version = "0.2.1"
 description = "Synchronize local git repo with remotes"
 authors = ["Alice Purcell <Alice.Purcell.39@gmail.com>"]
 