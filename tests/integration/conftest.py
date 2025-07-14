c41852ce642ce432526dbb7874a36a3f8cd79c55
Alice <alicederyn@gmail.com>
2025-07-14 07:50:43 +0100

    Commit after configuring user

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -14,12 +14,12 @@ def run_in_git_repo(tmp_path: Path) -> Iterator[None]:
     try:
         os.chdir(repo)
         subprocess.run(["git", "init", "-b", "main"], check=True)
-        subprocess.run(
-            ["git", "commit", "--allow-empty", "-m", "Initial commit"], check=True
-        )
         subprocess.run(["git", "config", "advice.detachedHead", "false"], check=True)
         subprocess.run(["git", "config", "user.email", "ci@example.com"], check=True)
         subprocess.run(["git", "config", "user.name", "CI"], check=True)
+        subprocess.run(
+            ["git", "commit", "--allow-empty", "-m", "Initial commit"], check=True
+        )
         yield
     finally:
         os.chdir(original_dir)
